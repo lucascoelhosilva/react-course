22592b0d5cdd222c4b48dcdca997346e2eea2f1a
Lucas Coelho <lucascoelhosilvacs@gmail.com>
2017-12-10 14:16:12 -0200

    Init

---
+++ exercicios_react/ex.1/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import { Primeiro, Segundo } from './component'
+import Family from './family';
+import Member from './member';
+
+ReactDOM.render(
+    <div>
+        <Family lastName="Silva">
+            <Member name="Guilherme"/>
+            <Member name="Rafael"/>
+            <Member name="Julia"/>
+        </Family>
+    </div>
+    , document.getElementById('app')
+)
---
+++ exercicios_react/ex.2/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import ClassComponent from './classComponent';
+import Field from './field';
+
+ReactDOM.render(
+    <Field initialValue="Teste"/>
+    , document.getElementById('app')
+)
