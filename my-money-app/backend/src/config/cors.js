220c719711c61b7634544bc64c42b3079877daea
Lucas Coelho <lucascoelhosilvacs@gmail.com>
2017-12-15 19:55:48 -0200

    my-money-app

---
+++ my-money-app/backend/src/config/cors.js
@@ -0,0 +1,6 @@
+module.exports = (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With')
+    next()
+}
